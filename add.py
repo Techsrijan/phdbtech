9d81c4d6cc3b70ea46fb462cb3c2104cb634f832
Ashwani <aswanibtech@gmail.com>
2021-10-18 08:17:49 +0530

    input program

---
+++ add.py
@@ -0,0 +1,22 @@
+#a,b=5,6
+'''
+gdsagdjsa
+sfhksdf
+dkasdkshd
+ssdsahd
+'''
+a=int(input("Enter Any Number "))
+print(type(a))
+b=int(input("Enetr Any number"))
+print(type(b))
+c=a+b
+print("Sum=",c)
+
+'''
++-*/
+area of circle
+area of traingle
+simple interest
+compound int
+degree cetigrade to farenheight
+'''
